038cbf1261d44b10656747587e7426334974e700
estri012 <estri.puji.lestari@mail.ugm.ac.id>
2023-01-27 15:47:26 +0700

    edit kedua

---
+++ assets/js/search.js
@@ -0,0 +1,10 @@
+"use strict"
+
+$(document).ready(function(){
+    $("#myInput").on("keyup", function() {
+      var value = $(this).val().toLowerCase();
+      $("#myDIV *").filter(function() {
+        $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+      });
+    });
+  });
