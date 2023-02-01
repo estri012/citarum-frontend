92d49e064dcaf1f54e8a7f8a461ace8654200c07
estri012 <estri.puji.lestari@mail.ugm.ac.id>
2023-02-01 09:12:20 +0700

    edit form kelahiran

--- assets/js/page/forms-advanced-forms.js
+++ assets/js/page/forms-advanced-forms.js
@@ -1,24 +1,7 @@
 "use strict";
 
-var cleavePN = new Cleave('.phone-number', {
-  phone: true,
-  phoneRegionCode: 'us'
-});
-var cleaveC = new Cleave('.currency', {
-  numeral: true,
-  numeralThousandsGroupStyle: 'thousand'
-});
-var cleavePC = new Cleave('.purchase-code', {
-  delimiter: '-',
-  blocks: [4, 4, 4, 4],
-  uppercase: true
-});
-var cleaveI = new Cleave('.invoice-input', {
-  prefix: 'INV',
-  delimiter: '-',
-  blocks: [10],
-  uppercase: true
-});
+
+
 var cleaveD = new Cleave('.datemask', {
   date: true,
   datePattern: ['Y', 'm', 'd']
